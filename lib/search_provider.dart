520f11c99ebc1bdf551dba4e621a7d2d6cfeee77
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-18 20:06:31 +0600

    update

--- lib/search_provider.dart
+++ lib/search_provider.dart
@@ -1,13 +1,23 @@
 
 import 'package:flutter_riverpod/legacy.dart';
 
-final searchProvider = StateNotifierProvider<SearchNotifier,String>((ref){
+final searchProvider = StateNotifierProvider<SearchNotifier,SearchState>((ref){
   return SearchNotifier();
 });
 
-class SearchNotifier extends StateNotifier<String> {
-  SearchNotifier() : super('');
+class SearchNotifier extends StateNotifier<SearchState> {
+  SearchNotifier() : super(SearchState(search: ''));
   void search(String quarry){
-    state = quarry;
+    state = state.copyWith(search: quarry);
+  }
+}
+
+class SearchState {
+  final String search;
+
+  SearchState({required this.search});
+
+  SearchState copyWith({String? search}){
+    return SearchState(search: search ?? this.search);
   }
 }
